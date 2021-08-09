1b1bf172cef992d829aa4d704da33beded8887ff
reregi <woxieric@live.cn>
2021-08-09 22:05:30 +0800

    update the popup

---
+++ popup.js
@@ -0,0 +1,12 @@
+const title = document.querySelector(".title"); //标题
+const uploader_name = document.querySelector(".uploader_name span"); //up主名
+const view = document.querySelector(".view"); //播放数
+const qrcode = document.querySelector(".qrCode_box img"); //二维码
+const sketch = document.querySelector(".sketch_box img"); //缩略图
+
+console.log(qrcode);
+console.log(sketch);
+
+title.addEventListener("click", function () {
+  console.log(1);
+});
